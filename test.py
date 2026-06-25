359e306adc812f8b4fd29807dc7747c7824f9cfe
Shetty Aadarsh Ravi <aadarshaiet.aiml@gmail.com>
2026-06-25 16:55:53 +0530

    git commands

---
+++ test.py
@@ -0,0 +1,22 @@
+#print("Hello world","world",sep="#")
+#print("hello world",end="")
+#print("hello world")
+#x="10.5"
+#y="s"
+#print("abc > abd")
+#x=int(input())
+#print(int(float(x)))
+#print("\test\ntesting")
+#print(r"\test\ntesting")
+#print("\\test\\ntesting")
+#print("""hello
+#      my name is
+#      jhon
+#      """)
+#x="hello"
+#print(x[0:3:2])
+#print(x[::-1])
+for i in range(0,10,2):
+    print(i)
+#while True:
+#   print("hello")
